0b6fd95b92cf9e44e7a2fec7e0ca355fe1b8d202
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-08 15:48:07 +0000

    Created a server file that ServerImpl calls to

---
+++ src/EchoServer.java
@@ -0,0 +1,14 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class EchoServer extends UnicastRemoteObject implements EchoService {
+    public EchoServer() throws RemoteException {
+
+    }
+
+    @Override
+    public String echo(String s) {
+        System.out.println("Replied to some client saying '" + s + "'");
+        return s;
+    }
+}
