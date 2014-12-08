4f638be0e68e0c4893c3a31cb74a59d7dbdf2925
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-12-08 16:49:07 +0000

    Added date and time

--- src/ex 2/DateServer.java
+++ src/ex 2/DateServer.java
@@ -2,14 +2,19 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Calendar;
 
-public class DataServer extends UnicastRemoteObject implements DateService {
-    public DataServer () throws RemoteException {
+public class DateServer extends UnicastRemoteObject implements DateService {
+    public DateServer () throws RemoteException {
 
     }
 
     @Override
     public void date() {
-        Calendar calc = new Calendar();
-        System.out.println(calc.getInstance().DATE + calc.getInstance().YEAR);
+        Calendar cal = Calendar.getInstance();
+        int year = Calendar.getInstance().get(cal.YEAR);
+        int month = Calendar.getInstance().get(cal.MONTH) + 1;
+        int day = Calendar.getInstance().get(cal.DAY_OF_MONTH);
+        int hour = Calendar.getInstance().get(cal.HOUR_OF_DAY);
+        int minutes = Calendar.getInstance().get(cal.MINUTE);
+        System.out.println(day + "/" + month + "/" + year + " " + hour + ":" + minutes);
     }
 }
